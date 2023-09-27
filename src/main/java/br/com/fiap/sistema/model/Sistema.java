68f097c3d937778edd337b2904acc825e60c2fda
aliciaguiradelo <alicia.guiradelo@outlook.com>
2023-09-27 19:51:00 -0300

    implementando classe Sistema

--- src/main/java/br/com/fiap/sistema/model/Sistema.java
+++ src/main/java/br/com/fiap/sistema/model/Sistema.java
@@ -1,15 +1,32 @@
 package br.com.fiap.sistema.model;
 
 import br.com.fiap.pessoa.model.Pessoa;
+import jakarta.persistence.*;
 
 import java.util.Collections;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "TB_SISTEMA", uniqueConstraints = {
+        @UniqueConstraint(name = "UK_SIGLA_TB_SISTEMA", columnNames = {"SIGLA_TB_SISTEMA"})
+})
 public class Sistema {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_TB_SISTEMA")
+    @SequenceGenerator(name = "SQ_TB_SISTEMA", sequenceName = "SQ_TB_SISTEMA")
+    @Column(name = "ID_TB_SISTEMA")
     private Long id;
+
+    @Column(name = "NOME_TB_SISTEMA", nullable = false)
     private String nome;
+
+    @Column(name = "SIGLA_TB_SISTEMA", nullable = false)
     private String sigla;
+
+    @ManyToMany(mappedBy = "sistemas", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @OrderBy("name asc")
     private Set<Pessoa> responsaveis = new LinkedHashSet<>();
 
     public Sistema(String nome, String sigla) {
